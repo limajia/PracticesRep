849ee7631250e9b26d3357f52abbdcbadf07bc7b
mlj <mlj@gmail.com>
2021-02-07 18:09:27 +0800

    surfaceView 缩放平移 及动画的理解

--- app/src/main/java/com/mlj/practicesrep/player/playerActivity.java
+++ app/src/main/java/com/mlj/practicesrep/player/playerActivity.java
@@ -4,6 +4,8 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +24,48 @@ public class playerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
         mSurfaceView = findViewById(R.id.surface_view);
+        View testButton = findViewById(R.id.mmmmmmmmmmmm);
+        //布局
+        View changeSizeBtn = findViewById(R.id.change_size_btn);
+        // 这种建立局部变量的方式 可以学习
+        // TODO: 2021/2/7
+        final int[] offset = {0};
+        changeSizeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                offset[0] += 20;
+                ViewGroup.LayoutParams layoutParams = mSurfaceView.getLayoutParams();
+                layoutParams.width = 400 + offset[0];
+                layoutParams.height = 200 + offset[0];
+                mSurfaceView.setLayoutParams(layoutParams);
+            }
+        });
+        //scale
+        View sclebtn = findViewById(R.id.scale_btn);
+        final float[] offset1 = {1};
+        sclebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                offset1[0] += 0.1;
+                mSurfaceView.setScaleX(offset1[0]);
+                mSurfaceView.setScaleY(offset1[0]);
+
+                testButton.setScaleX(offset1[0]);
+                testButton.setScaleY(offset1[0]);
+            }
+        });
+        //alpha 是无效
+        View alphabtn = findViewById(R.id.alpha_btn);
+        final float[] offset2 = {1};
+        alphabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                offset2[0] -= 0.1;
+//                mSurfaceView.setAlpha(offset2[0]);
+                mSurfaceView.animate().translationYBy(-10).setDuration(1000).start(); // 这个是有效的
+                testButton.animate().translationYBy(-10).setDuration(1000).start(); // 这个是有效的
+            }
+        });
         mSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(@NonNull SurfaceHolder holder) {
