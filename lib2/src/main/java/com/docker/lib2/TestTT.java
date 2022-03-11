4a70a46c0b0b4b8874b8ce3f450cde74589d9bca
lijia19 <lijia19@staff.weibo.com>
2022-03-11 14:37:21 +0800

    1.重写规则的理解补充 2. try 不catch

---
+++ lib2/src/main/java/com/docker/lib2/TestTT.java
@@ -0,0 +1,15 @@
+package com.docker.lib2;
+
+public class TestTT {
+    public static void main(String[] args) {
+        try {
+            int c = 3 / 6;
+            int b = 33 / c;
+        }
+        catch (Exception e) {
+
+        }finally {
+            System.out.println("结果");
+        }
+    }
+}
