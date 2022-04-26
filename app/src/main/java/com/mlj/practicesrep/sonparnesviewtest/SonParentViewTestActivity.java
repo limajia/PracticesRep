cb811760af9fdc65b78bab83f1b83322c81a4718
lijia19 <lijia19@staff.weibo.com>
2022-04-27 00:01:42 +0800

    换成surfaceView后的理解

--- app/src/main/java/com/mlj/practicesrep/sonparnesviewtest/SonParentViewTestActivity.java
+++ app/src/main/java/com/mlj/practicesrep/sonparnesviewtest/SonParentViewTestActivity.java
@@ -1,10 +1,14 @@
 package com.mlj.practicesrep.sonparnesviewtest;
 
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mlj.practicesrep.R;
@@ -14,7 +18,7 @@ public class SonParentViewTestActivity extends AppCompatActivity {
     private LinearLayout mParentView;
     private LinearLayout mSonView;
     private LinearLayout mRootView;
-    private LinearLayout mScreenWidthSize;
+    private SurfaceView mScreenWidthSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,26 @@ public class SonParentViewTestActivity extends AppCompatActivity {
         mSonView = findViewById(R.id.sonView);
         mRootView = findViewById(R.id.rootView);
         mScreenWidthSize = findViewById(R.id.screen_width_size);
+        mScreenWidthSize.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+                // 控件换成surfaceVIew的话，surface的宽度也是可以超过屏幕宽度的,但是鸿蒙的不可以超过屏幕宽度
+                // 如果通过setFixedSize，修改surface的大小后，会通过strength拉伸的形式，缩放到surfaceView大小范围内
+                // 如果surface的大小和surfaceView的大小相同，那该多大就多大，该超出屏幕就超出屏幕
+                System.out.println("docker13:surfaceChanged width=" + width);//1300
+                System.out.println("docker13:surfaceChanged height=" + height);//200
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+            }
+        });
         mRootView.postDelayed(new Runnable() {
             @Override
             public void run() {
