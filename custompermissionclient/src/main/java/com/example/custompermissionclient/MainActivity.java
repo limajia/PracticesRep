8e9b4246533d45d82aa816fceeda4832a282e1d1
malijia <malijia@apusapps.com>
2023-07-26 16:22:54 +0800

    自定义Dangerous权限

--- custompermissionclient/src/main/java/com/example/custompermissionclient/MainActivity.java
+++ custompermissionclient/src/main/java/com/example/custompermissionclient/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.custompermissionclient;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -24,10 +26,28 @@ public class MainActivity extends AppCompatActivity {
             int i = ActivityCompat.checkSelfPermission(this, "com.mlj.practicesrep.permission.playerpermission");
             System.out.println("docker345: result=" + i);
 
-            //有权限了可以跳转
-            Intent intent = new Intent();
+            //normal权限，声明了就有权限了，可以直接跳转
+            /*Intent intent = new Intent();
             intent.setComponent(new android.content.ComponentName("com.mlj.practicesrep", "com.mlj.practicesrep.player.PlayerActivity"));
-            startActivity(intent);
+            startActivity(intent);*/
+
+            //dangerous权限，需要用户同意权限，才可以跳转
+            if (i == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, new String[]{"com.mlj.practicesrep.permission.playerpermission"}, 1000);
+            }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000) {
+                //dangerous权限，需要用户同意权限，才可以跳转
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent();
+                    intent.setComponent(new android.content.ComponentName("com.mlj.practicesrep", "com.mlj.practicesrep.player.PlayerActivity"));
+                    startActivity(intent);
+                }
+        }
     }
 }
