18b08c044c0e65349ed5d52fe7b988c40344dd30
malijia <malijia@apusapps.com>
2023-12-25 18:51:48 +0800

    GridLayout布局调整

--- app/src/main/java/com/mlj/practicesrep/snaphelptest/SnapHelpTestActivity.java
+++ app/src/main/java/com/mlj/practicesrep/snaphelptest/SnapHelpTestActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,6 +26,46 @@ public class SnapHelpTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snap_help_test);
         initView();
+        initGridLayoutManagerRv();
+    }
+
+    private void initGridLayoutManagerRv() {
+        RecyclerView recyclerViewForGridLayoutManager = findViewById(R.id.recyclerViewForGridLayoutManager);
+        recyclerViewForGridLayoutManager.setLayoutManager(new GridLayoutManager(this, 3, LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewForGridLayoutManager.setAdapter(new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                TextView inflate = new TextView(parent.getContext());
+                inflate.setLayoutParams(new ViewGroup.LayoutParams(400, 100));
+                RecyclerView.ViewHolder viewHolder = new RecyclerView.ViewHolder(inflate) {
+                };
+                //获得随机颜色值
+                inflate.setBackgroundColor(Color.argb(255, (int) (Math.random() * 255),
+                        (int) (Math.random() * 255), (int) (Math.random() * 255)));
+                return viewHolder;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+                TextView itemView = (TextView) holder.itemView;
+                itemView.setText(position + "");
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        new Handler().post(() -> {
+                            //这里需要延迟一下，不然会出现滑动不到位的情况
+                            recyclerViewForGridLayoutManager.smoothScrollToPosition(position);
+                        });
+                    }
+                });
+            }
+
+            @Override
+            public int getItemCount() {
+                return 50;
+            }
+        });
     }
 
     private void initView() {
